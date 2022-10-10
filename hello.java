2a01fd17f4420f4e1d8e7d3f26e64227d56bcb24
Tanmoy <89120102+ArrowA1@users.noreply.github.com>
2022-10-10 05:35:19 +0530

    updated `main()` function to `void` return type

--- hello.java
+++ hello.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 public class Hello
 {
-  public static int main(String[] args)
+  public static void main(String[] args)
   {
     Scanner sc = new Scanner(System.in);
     System.out.printf("Name: ");
     String name = sc.nextLine();
     
     System.out.println("Hello " + name + "!");
-   
-    return 0;
   }
 }   
