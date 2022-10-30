56c1bdd071c84682109fc6da0279687cc2f00331
Tanmoy <89120102+ArrowA1@users.noreply.github.com>
2022-10-31 02:57:26 +0530

    remove "Name: " prompt

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public class Hello
   public static void main(String[] args)
   {
     Scanner sc = new Scanner(System.in);
-    System.out.printf("Name: ");
+    
     String name = sc.nextLine();
     
     System.out.println("Hello " + name + "!");
