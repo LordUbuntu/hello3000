a71d0d29496993fa94dff2d8a5e0c65bd8e1237b
Jacobus Burger <therealjacoburger@gmail.com>
2022-11-24 14:10:39 -0800

    corrected faulty rust submission

--- hello.rs
+++ hello.rs
@@ -1,4 +1,8 @@
-fn main(){
-  let mut name = String::new();
-  print!("Hello, {}!", name);
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut name = String::new();
+    io::stdin().read_line(&mut name);
+    println!("Hello, {}!", name);
+    Ok(())
 }
