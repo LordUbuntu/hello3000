69ca1faf8143be3aef7d610989ab22ec08e71d27
Jacobus Burger <therealjacoburger@gmail.com>
2024-04-03 14:57:56 -0700

    cleaned up zig

--- hello.zig
+++ hello.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
+const stdout = std.io.getStdOut.writer;
+const stdin = std.io.getStdIn.reader;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    const stdin = std.io.getStdIn().reader();
     var name: [2048]u8 = undefined;
-
     _ = try stdin.readUntilDelimiter(&name, '\n');
     try stdout.print("Hello, {s}!\n", .{name});
 }
