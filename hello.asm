dffab7fa3b2d030629d3f1cb96c9a52d95635418
Jacobus Burger <therealjacoburger@gmail.com>
2024-05-15 16:10:56 -0700

    added comments to clarify

--- hello.asm
+++ hello.asm
@@ -1,16 +1,15 @@
 ; Created by Jacobus Burger (2023)
-; Hello World in assembly, one of the first programs I ever wrote!
+; x86_64 Assembly
 section .data
 	msg db "你好, 世界!", 10, "Bonjour, le monde!", 10, "Hello, World!", 10, 0
 	len equ $ - msg
 section .text
 	global _start
 _start:
-	mov edx, len
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
-	int 80h
-	mov eax, 1
-	int 80h
-  ret
+	mov edx, len    ; set length of message to write
+	mov ecx, msg    ; set message to write
+	mov ebx, 1      ; set file descriptor to stdout
+	mov eax, 4      ; set system call number to sys_write
+	int 0x80        ; system interrupt (call kernel)
+	mov eax, 1      ; set system call number to sys_exit
+	int 80h         ; system interrupt (call kernel)
