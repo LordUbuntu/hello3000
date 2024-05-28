fbb7099d57288b85aa8d0add1f9a85892bceb102
Jacobus Burger <therealjacoburger@gmail.com>
2024-05-27 23:24:00 -0700

    update info

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
+// Jacobus Burger (2023)
+// C99
 #include <stdio.h>
 
 
 int main(void) {
-        char name[2048];
+        char name[4096];
         scanf("%[^\n]s", name);  // note: gcc specific scanf args
         printf("Hello, %s!\n", name);
 }
