63564cdb85cb62671f7e2287e9b276ec9d17a429
Jacobus Burger <therealjacoburger@gmail.com>
2024-06-30 19:52:00 -0700

    add info

--- hello.scm
+++ hello.scm
@@ -1,4 +1,6 @@
-; standard scheme, version 5f3
+; Jacobus Burger (2023)
+; Scheme R7
+; https://www.scheme.org/
 (define (main) ; note that scheme standard has no format or printf
   (define name (read-line)) 
   (display (string-append "Hello, " name "!"))
