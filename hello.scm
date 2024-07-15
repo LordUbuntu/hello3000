c6d67dbbe27a7a92147d41a7a2836bd30b3c1a49
Jacobus Burger <therealjacoburger@gmail.com>
2024-07-14 21:58:13 -0700

    added comments

--- hello.scm
+++ hello.scm
@@ -2,8 +2,7 @@
 ; Scheme R7
 ; https://www.scheme.org/
 (define (main) ; note that scheme standard has no format or printf
-  (define name (read-line)) 
-  (display (string-append "Hello, " name "!"))
-  (newline))
+  (define name (read-line))  ; get name
+  (display (string-append "Hello, " name "!")) (newline))  ; print msg
 
 (main)
